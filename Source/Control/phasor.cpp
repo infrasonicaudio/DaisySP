62ec3cd0caf79dc7538c137d36bb7e046612f093
Nick Donaldson <ndonald2@gmail.com>
2023-03-27 15:59:20 -0600

    Faster phasor, unit range and no negative freq checks

--- Source/Control/phasor.cpp
+++ Source/Control/phasor.cpp
@@ -6,22 +6,18 @@ using namespace daisysp;
 
 void Phasor::SetFreq(float freq)
 {
-    freq_ = freq;
-    inc_  = (TWOPI_F * freq_) / sample_rate_;
+    freq_ = fmax(freq, 0.0f);
+    inc_  = freq_ / sample_rate_;
 }
 
 float Phasor::Process()
 {
     float out;
-    out = phs_ / TWOPI_F;
+    out = phs_;
     phs_ += inc_;
-    if(phs_ > TWOPI_F)
+    if(phs_ > 1.0f)
     {
-        phs_ -= TWOPI_F;
-    }
-    if(phs_ < 0.0f)
-    {
-        phs_ = 0.0f;
+        phs_ -= 1.0f;
     }
     return out;
 }
